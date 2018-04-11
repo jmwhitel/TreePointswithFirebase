4e05f43ce98035183ea4ef33ca23fe8f68a0a342
Rob McMenamin <robmcmen@gmail.com>
2018-04-10 21:05:38 -0400

    Added code to loop through datasnapshot

--- app/src/main/java/whiteley/treepointswithfirebase/TreeDBViewer.java
+++ app/src/main/java/whiteley/treepointswithfirebase/TreeDBViewer.java
@@ -1,32 +1,26 @@
 package whiteley.treepointswithfirebase;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class TreeDBViewer extends AppCompatActivity {
@@ -40,18 +34,33 @@ public class TreeDBViewer extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tree_pointsdb);
+        setContentView(R.layout.activity_tree_viewer);
         database = FirebaseDatabase.getInstance();
-        databaseReference = FirebaseDatabase.getInstance().getReference("Tree Point");
-
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        Query allTrees = databaseReference.child("Tree Point");
 
         listView = (ListView) findViewById(R.id.listViewTrees);
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mTreeSpecies);
         listView.setAdapter(arrayAdapter);
-        myRef.addChildEventListener(new ChildEventListener() {
+
+        allTrees.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DataSnapshot dataSnapshot1 = dataSnapshot;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    System.out.println(snapshot);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("Viewer","ERORR");
+            }
+        });
+        allTrees.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
+                DataSnapshot dataSnapshot1 =dataSnapshot;
                 String value = dataSnapshot.getValue(String.class);
                 mTreeSpecies.add(value);
                 arrayAdapter.notifyDataSetChanged();
